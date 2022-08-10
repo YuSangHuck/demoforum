f19c571380bcf81b79a1027b8ad2ca98faa2fae6
yusanghuck <g01099387636@gmail.com>
2022-08-11 00:16:47 +0900

    init

---
+++ src/test/java/com/demo/demoforum/DemoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demoforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
