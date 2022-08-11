70318d41685e68514328c40468c4ba8370dcfa2f
yusanghuck <g01099387636@gmail.com>
2022-08-11 21:52:46 +0900

    Add paging to question

--- src/main/java/com/demo/demoforum/feature/question/QuestionController.java
+++ src/main/java/com/demo/demoforum/feature/question/QuestionController.java
@@ -3,13 +3,13 @@ package com.demo.demoforum.feature.question;
 import com.demo.demoforum.feature.answer.AnswerFormDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RequestMapping("/questions")
 @Controller
@@ -20,9 +20,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questions = questionService.getQuestions();
-        model.addAttribute("questions", questions);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = questionService.getQuestions(page);
+        model.addAttribute("paging", paging);
         return "questions/list";
     }
 
--- src/main/java/com/demo/demoforum/feature/question/QuestionService.java
+++ src/main/java/com/demo/demoforum/feature/question/QuestionService.java
@@ -3,8 +3,13 @@ package com.demo.demoforum.feature.question;
 import com.demo.demoforum.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,6 +20,13 @@ public class QuestionService {
 
     public List<Question> getQuestions() {
         return this.questionRepository.findAll();
+    }
+
+    public Page<Question> getQuestions(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("id"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAll(pageable);
     }
 
     public Question getQusetion(Long id) {
