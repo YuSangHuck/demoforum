f65e06d95c4a348ea66c8b5ecd630a791f9ce74d
yusanghuck <g01099387636@gmail.com>
2022-08-15 03:16:51 +0900

    update

--- main.tf
+++ main.tf
@@ -1,23 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region  = "ap-northeast-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
