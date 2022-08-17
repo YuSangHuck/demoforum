1820b89ac6dccc5bf66d318124ceb6c6ae772814
yusanghuck <g01099387636@gmail.com>
2022-08-17 18:38:07 +0900

    aws lambda: java17 -> java11

--- src/main/java/com/demo/demoforum/exception/DataNotFoundException.java
+++ src/main/java/com/demo/demoforum/exception/DataNotFoundException.java
@@ -3,11 +3,8 @@ package com.demo.demoforum.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serial;
-
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
 public class DataNotFoundException extends RuntimeException {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public DataNotFoundException(String msg) {
