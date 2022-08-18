c154f610db794251d1b04523f10f1b5c2dac31b7
yusanghuck <g01099387636@gmail.com>
2022-08-19 01:01:39 +0900

    Update SecurityConfig
    - session: ? -> STATELESS
    - csrf: HttpSessionCsrfToken -> CookieCsrfToken

--- src/main/java/com/demo/demoforum/config/SecurityConfig.java
+++ src/main/java/com/demo/demoforum/config/SecurityConfig.java
@@ -9,9 +9,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -33,6 +35,13 @@ public class SecurityConfig {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/users/signout"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
+        ;
+
+//        FIXME 인증쪽 공부후 다시
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .csrf().csrfTokenRepository(new CookieCsrfTokenRepository())
         ;
         return http.build();
     }
