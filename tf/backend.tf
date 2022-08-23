67f0ad8c6f4a1da037df8d93767ba875064dcb33
yusanghuck <g01099387636@gmail.com>
2022-08-23 15:21:11 +0900

    Add backend(s3,dynamodb)

---
+++ tf/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-demo-forum-state"
+    key            = "terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "tf-demo-forum-state-lock"
+  }
+}
